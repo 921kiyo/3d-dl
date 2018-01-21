4be91faeb822a60a43709ae6d5760e6bb4f40e42
MSI\weihan <samuelong168@gmail.com>
2018-01-21 12:05:09 +0000

    added BlenderNode class and subclasses, subclass Blender Mesh, expanded functionality

--- src/rendering/BlenderObjects.py
+++ src/rendering/BlenderObjects.py
@@ -2,6 +2,7 @@ import bpy
 import math
 import random
 import mathutils as mathU
+import itertools
 
 def random_color():
 	return(random.random(), random.random(), random.random())
@@ -34,6 +35,142 @@ def random_cartesian_coords(mux, muy, muz, sigma, lim):
 	y = min(random.gauss(muy,sigma), lim)
 	z = min(random.gauss(muz,sigma), lim)
 	return x,y,z
+
+class BlenderNode(object):
+	def __init__(self, node_tree, type, reference=None):
+		self.node_tree = node_tree
+		self.type=type
+		if reference is None:
+			self.reference = self.node_tree.nodes.new(type=type)
+		else:
+			self.reference = reference
+	
+	def get_input(self,idx):
+		if (type(idx) is int) and ( idx > len(self.reference.inputs.keys()) ):
+			return None
+		if (type(idx) is str) and ( idx not in self.reference.inputs.keys()):
+			return None
+		return self.reference.inputs[idx]
+	
+	def get_output(self,idx):
+		if (type(idx) is int) and ( idx > len(self.reference.outputs.keys()) ):
+			return None
+		if (type(idx) is str) and ( idx not in self.reference.outputs.keys()):
+			return None
+		return self.reference.outputs[idx]
+	
+	def set_input(self,idx,val):
+		input = self.get_input(idx)
+		if input is None:
+			return False
+		input.default_value = val
+		return True
+
+class BlenderMixShaderNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderMixShaderNode, self).__init__(node_tree,'ShaderNodeMixShader',reference)
+
+	def get_shader1_input(self):
+		return self.get_input(1)
+	
+	def get_shader2_input(self):
+		return self.get_input(2)	
+	
+	def get_shader_output(self):
+		return self.get_output('Shader')
+	
+	def set_fac(self,fac):
+		if fac > 1.0:
+			fac = 1.0
+		self.set_input('Fac',fac)
+
+class BlenderMaterialOutputNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderMaterialOutputNode, self).__init__(node_tree,None,reference)
+
+	def get_surface_input(self):
+		return self.get_input('Surface')
+		
+class BlenderDiffuseBSDFNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderDiffuseBSDFNode, self).__init__(node_tree,'ShaderNodeBsdfDiffuse',reference)
+	
+	def set_color(self,r,g,b,a):
+		args = (r,g,b,a)
+		for i,arg in enumerate(args):
+			if arg > 1.0:
+				args[i] = 1.0
+		self.set_input('Color',args)
+	
+	def set_roughness(self, r):
+		if r > 1.0:
+			r = 1.0
+		self.set_input('Roughness',r)
+	
+	def get_bsdf_output(self):
+		return self.get_output('BSDF')
+	
+	def get_color_input(self):
+		return self.get_input('Color')
+
+class BlenderGlossyBSDFNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderGlossyBSDFNode, self).__init__(node_tree,'ShaderNodeBsdfGlossy',reference)
+	
+	def set_color(self,r,g,b,a):
+		args = (r,g,b,a)
+		for i,arg in enumerate(args):
+			if arg > 1.0:
+				args[i] = 1.0
+		self.set_input('Color',args)
+	
+	def set_roughness(self, r):
+		if r > 1.0:
+			r = 1.0
+		self.set_input('Roughness',r)
+	
+	def get_bsdf_output(self):
+		return self.get_output('BSDF')
+	
+	def get_color_input(self):
+		return self.get_input('Color')
+
+		
+class BlenderImageTextureNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderImageTextureNode, self).__init__(node_tree,'ShaderNodeTexImage',reference)
+	
+	def set_projection(self, projection):
+		try:
+			self.reference.projection = projection
+		except TypeError:
+			return False
+		return True
+		
+	def set_image(self, image):
+		try:
+			self.reference.image = image
+		except TypeError:
+			return False
+		return True
+	
+	def get_vector_input(self):
+		return self.get_input('Vector')
+	
+	def get_color_output(self):
+		return self.get_output('Color')
+
+		
+class BlenderTexCoordNode(BlenderNode):
+	def __init__(self, node_tree, reference=None):
+		super(BlenderTexCoordNode, self).__init__(node_tree,'ShaderNodeTexCoord',reference)
+	
+	def get_UV_output(self):
+		return self.get_output('UV')
+		
+	def get_Generated_output(self):
+		return self.get_output('Generated')
+
 	
 class BlenderObject(object):
 	def __init__(self, location=(0,0,0), orientation=(0,0,0,0), scale=(1,1,1), reference=None):
@@ -73,7 +210,6 @@ class BlenderObject(object):
 			self.reference.data.materials.append(mat) #add the material to the object
 		self.reference.data.materials[0].diffuse_color = (r, g, b) #change color
 	
-	
 	def delete(self):
 		# deselect all
 		bpy.ops.object.select_all(action='DESELECT')
@@ -82,14 +218,83 @@ class BlenderObject(object):
 		# remove it
 		bpy.ops.object.delete() 
 
-class BlenderCube(BlenderObject):
+class BlenderMesh(BlenderObject):
+	def __init__(self, **kwargs):
+		super(BlenderMesh, self).__init__(**kwargs)
+		self.nodes = {}
+		self.links = None
+		self.node_tree = None
+		self.material = None
+		self.setup_node_tree()
+	
+	def setup_node_tree(self):
+	
+		obj_name = self.reference.name
+		if(len(self.reference.data.materials)==0):
+			mat = bpy.data.materials.new(name="Mat"+"_"+obj_name) #set new material to variable
+			self.reference.data.materials.append(mat) #add the material to the object
+		self.material = self.reference.data.materials[0]
+		self.reference.data.materials[0].use_nodes = True
+		
+		self.node_tree = self.material.node_tree
+		self.nodes['node_mat'] = BlenderMaterialOutputNode(self.node_tree, reference=self.node_tree.nodes['Material Output'])
+		self.nodes['node_diff'] = BlenderDiffuseBSDFNode(self.node_tree, reference=self.node_tree.nodes['Diffuse BSDF'])
+		self.nodes['node_gloss'] = BlenderGlossyBSDFNode(self.node_tree)
+		self.nodes['node_mix'] = BlenderMixShaderNode(self.node_tree)
+		self.links = self.node_tree.links
+		self.links.new(self.nodes['node_mix'].get_shader_output(), self.nodes['node_mat'].get_surface_input())
+		self.links.new(self.nodes['node_diff'].get_bsdf_output(), self.nodes['node_mix'].get_shader1_input())
+		self.links.new(self.nodes['node_gloss'].get_bsdf_output(), self.nodes['node_mix'].get_shader2_input())
+	
+	def set_diffuse(self,color=(0.5,0.5,0.5,1),rough=0.5):
+		self.nodes['node_diff'].set_color(*color)
+		self.nodes['node_diff'].set_roughness(rough)
+		
+	def set_gloss(self,color=(0.5,0.5,0.5,1),rough=0.5):
+		self.nodes['node_gloss'].set_color(*color)
+		self.nodes['node_gloss'].set_roughness(rough)
+	
+	def set_mixer(self,factor):
+		self.nodes['node_mix'].set_fac(factor)
+	
+	def add_image_texture(self, image_path, projection='FLAT', mapping ='UV'):
+	
+		if not 'node_imgtex' in self.nodes.keys():
+			self.nodes['node_imgtex'] = BlenderImageTextureNode(self.node_tree)
+		if not 'node_texcoord' in self.nodes.keys():
+			self.nodes['node_texcoord'] = BlenderTexCoordNode(self.node_tree)
+			
+		try:
+			img = bpy.data.images.load(image_path)
+		except:
+			return False
+		
+		success = False
+		success = self.nodes['node_imgtex'].set_projection(projection) and self.nodes['node_imgtex'].set_image(img)
+		if not success:
+			return success
+		
+		if mapping not in ['UV','Generated']:
+			return False
+		
+		vector = None
+		if mapping=='Generated':
+			vector = self.nodes['node_texcoord'].get_Generated_output()
+		else:
+			vector = self.nodes['node_texcoord'].get_UV_output()
+		self.links.new(vector, self.nodes['node_imgtex'].get_vector_input())
+		self.links.new(self.nodes['node_imgtex'].get_color_output(), self.nodes['node_diff'].get_color_input())
+		self.links.new(self.nodes['node_imgtex'].get_color_output(), self.nodes['node_gloss'].get_color_input())
+		
+		
+class BlenderCube(BlenderMesh):
 	def __init__(self, **kwargs):
 		super(BlenderCube, self).__init__(**kwargs)
 	
 	def blender_create_operation(self,location):
 		bpy.ops.mesh.primitive_cube_add(location=location)
 
-class BlenderPlane(BlenderObject):
+class BlenderPlane(BlenderMesh):
 	def __init__(self, **kwargs):
 		super(BlenderPlane, self).__init__(**kwargs)
 	
@@ -114,9 +319,15 @@ class BlenderCamera(BlenderObject):
 		self.set_rot(rot_angle, rot_axis[0], rot_axis[1], rot_axis[2])
 
 class BlenderLamp(BlenderObject):
-	def __init__(self, obj_reference, obj_data):
+	def __init__(self, obj_reference):
 		super(BlenderLamp, self).__init__(reference=obj_reference)
-		self.data = obj_data
+		self.data = self.reference.data
+	
+	def blender_create_operation(self,location):
+		bpy.ops.object.lamp_add(location=location)
+	
+	def set_size(self,size):
+		self.data.shadow_soft_size = size
 	
 	def set_brightness(self,strength):
 		self.data.use_nodes = True
@@ -165,7 +376,7 @@ class BlenderScene(object):
 		self.objects_fixed.append(object)
 	
 	def add_object_unfixed(self,object):
-		self.ojbects_unfixed.append(object)
+		self.objects_unfixed.append(object)
 	
 	def add_lamp(self,lamp):
 		self.lamp = lamp
@@ -180,6 +391,7 @@ class BlenderScene(object):
 		self.objects_unfixed = []
 	
 	def set_render(self):
+		self.data.cycles.film_transparent = True
 		self.data.cycles.max_bounces = 3
 		self.data.cycles.min_bounces = 1
 		self.data.cycles.transparent_max_bounces = 3
@@ -190,4 +402,8 @@ class BlenderScene(object):
 		self.data.render.tile_y = 512
 		self.data.render.resolution_x = 720
 		self.data.render.resolution_y = 720
+	
+	def render_to_file(self,filepath):
+		self.data.render.filepath = filepath
+		bpy.ops.render.render( write_still=True )
 		
