a97fcdbcea5117b53e83a1c0a2dbebfa254c2211
MSI\weihan <samuelong168@gmail.com>
2018-01-26 15:22:32 +0000

    update camera randomization to include spin

--- src/rendering/BlenderObjects.py
+++ src/rendering/BlenderObjects.py
@@ -253,6 +253,9 @@ class BlenderObject(object):
 		q = to_quaternion(w,x,y,z)
 		self.reference.rotation_quaternion = q
 	
+	def get_rot(self):
+		return self.reference.rotation_quaternion
+	
 	def rotate(self, w,x,y,z):
 		self.reference.rotation_mode = 'QUATERNION'
 		q = to_quaternion(w,x,y,z)
@@ -358,6 +361,7 @@ class BlenderPlane(BlenderMesh):
 class BlenderImportedShape(BlenderMesh):
 	def __init__(self, **kwargs):
 		super(BlenderImportedShape, self).__init__(**kwargs)
+		bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
 	
 	def blender_create_operation(self,location,obj_path=None):
 		assert obj_path is not None, "Required keyword argument for importing shape: obj_path=[filepath]"
@@ -366,6 +370,15 @@ class BlenderImportedShape(BlenderMesh):
 class BlenderCamera(BlenderObject):
 	def __init__(self, reference, **kwargs):
 		super(BlenderCamera, self).__init__(reference=reference, **kwargs)
+	
+	def spin(self,angle):
+		q = self.get_rot()
+		focal_origin = mathU.Vector([0,0,-1])
+		T = q.to_matrix()
+		#T.invert()
+		focal_axis = T*focal_origin
+		focal_axis.normalize()
+		self.rotate(angle, *focal_axis)
 	
 	def face_towards(self,x,y,z):
 		# vector of target w.r.t camera
@@ -379,11 +392,13 @@ class BlenderCamera(BlenderObject):
 		rot_angle = math.degrees(math.acos(rot_origin.dot(target)))
 		# set rotation quaternion
 		self.set_rot(rot_angle, rot_axis[0], rot_axis[1], rot_axis[2])
-
+		
 class BlenderLamp(BlenderObject):
 	def __init__(self, obj_reference):
 		super(BlenderLamp, self).__init__(reference=obj_reference)
 		self.data = self.reference.data
+		self.default_brightness = 0.0
+		self.default_size = 0.0
 	
 	def blender_create_operation(self,location):
 		bpy.ops.object.lamp_add(location=location)
@@ -394,7 +409,64 @@ class BlenderLamp(BlenderObject):
 	def set_brightness(self,strength):
 		self.data.use_nodes = True
 		self.data.node_tree.nodes["Emission"].inputs["Strength"].default_value = strength
+	
+	def turn_off(self):
+		self.reference.layers[1] = True
+		self.reference.layers[0] = False	
+	
+	def turn_on(self):
+		self.reference.layers[0] = True
+		self.reference.layers[1] = False
+	
+	def face_towards(self,x,y,z):
+		# vector of target w.r.t camera
+		target = mathU.Vector([x,y,z]) - mathU.Vector(self.reference.location)
+		target.normalize()
+		# rotational origin of camera is (0,0,-1) for some reason
+		rot_origin = mathU.Vector([0,0,-1])
+		rot_origin.normalize()
+		# get the rotational axis and angle by crossing the two vectors
+		rot_axis = rot_origin.cross(target)
+		rot_angle = math.degrees(math.acos(rot_origin.dot(target)))
+		# set rotation quaternion
+		self.set_rot(rot_angle, rot_axis[0], rot_axis[1], rot_axis[2])
+	
+	def random_lighting_conditions(self, reference_location=(0.0,0.0,0.0), location_variance=1.0):
+		loc = random_cartesian_coords(0.0,0.0,0.0,location_variance,6.0)
+		self.face_towards(*loc)
+		self.set_brightness(random.gauss(self.default_brightness, 0.1*self.default_brightness))
+	
+	
+class BlenderSun(BlenderLamp):
+	def __init__(self, obj_reference, default_brightness=15.0, default_size=0.1):
+		super(BlenderSun, self).__init__(obj_reference)
+		self.data.type = 'SUN'
+		self.default_brightness = default_brightness
+		self.default_size = default_size
+		self.set_brightness(default_brightness)
+		self.set_size(default_size)
 
+
+class BlenderArea(BlenderLamp):
+	def __init__(self, obj_reference, default_brightness=500.0, default_size=5.0):
+		super(BlenderArea, self).__init__(obj_reference)
+		self.data.type = 'AREA'
+		self.default_brightness = default_brightness
+		self.default_size = default_size
+		self.set_brightness(default_brightness)
+		self.set_size(default_size)
+
+
+class BlenderPoint(BlenderLamp):
+	def __init__(self, obj_reference, default_brightness=5000.0, default_size=5.0):
+		super(BlenderPoint, self).__init__(obj_reference)
+		self.data.type = 'POINT'
+		self.default_brightness = default_brightness
+		self.default_size = default_size
+		self.set_brightness(default_brightness)
+		self.set_size(default_size)	
+
+		
 class BlenderRoom(object):
 	def __init__(self,radius):
 		self.walls = []
@@ -464,6 +536,7 @@ class BlenderScene(object):
 		self.data.render.tile_y = 512
 		self.data.render.resolution_x = 720
 		self.data.render.resolution_y = 720
+		self.data.render.resolution_percentage = 85
 	
 	def render_to_file(self,filepath):
 		self.data.render.filepath = filepath
--- src/rendering/example2.py
+++ src/rendering/example2.py
@@ -8,6 +8,7 @@ import math
 import mathutils
 import bpy
 import csv
+import os
 
 bpy.context.scene.render.engine = 'CYCLES'
 
@@ -28,17 +29,18 @@ cube = bo.BlenderCube(reference=bpy.data.objects['Cube'])
 cube.delete()
 
 # required file paths for the script to run
-obj_path = 'D:\\PycharmProjects\\Lobster\\src\\rendering\\9562dwzzz4sg-BottleBeerCorona\\Corona\\Corona.obj'
-texture_path = 'D:\\PycharmProjects\\Lobster\\src\\rendering\\9562dwzzz4sg-BottleBeerCorona\\Corona\\BotellaText.jpg'
-csv_path = 'D:\\PycharmProjects\\Lobster\\src\\rendering\\9562dwzzz4sg-BottleBeerCorona\\Corona\\render\\camera.csv'
+obj_path = 'D:\\PycharmProjects\\Product3\\Yoghurt\\Yoghurt.obj'
+texture_path = 'D:\\PycharmProjects\\Product3\\Yoghurt\\Yoghurt.jpg'
+render_folder = 'D:\\PycharmProjects\\Product3\\Yoghurt\\render'
+csv_path = os.path.join(render_folder,'camera.csv')
 	
 product = bo.BlenderImportedShape(obj_path=obj_path, location=(-1,0,-1) ,orientation=(0,0,1,0))
 #give image texture path 
 product.add_image_texture(texture_path)
 product.set_diffuse(color=(1,0,0,1),rough=0.1)
 product.set_gloss(rough=0.1)
-product.set_mixer(0.6)
-product.set_scale((.1,.1,.1))
+product.set_mixer(0.3)
+product.set_scale((.65,.65,.65))
 product.toggle_smooth()
 
 # Create a cube
@@ -50,41 +52,42 @@ cube2.set_mixer(0.3)
 
 # Fetch the camera and lamp
 cam = bo.BlenderCamera(bpy.data.objects['Camera'])
-lamp = bo.BlenderLamp(bpy.data.objects['Lamp'])
+lamp = bo.BlenderPoint(bpy.data.objects['Lamp'])
 
 # Create a 2nd lamp
-lamp2 = bo.BlenderLamp(None)
-lamp.set_brightness(500.00)
-lamp.set_size(5.0)
-lamp2.set_brightness(500.00)
-lamp2.set_size(5.0)
+lamp2 = bo.BlenderPoint(None)
+
+# Create Sun
+#lamp3 = bo.BlenderSun(None)
 
 # instantiate scene
 scene = bo.BlenderScene(bpy.data.scenes[0])
 scene.set_render()
 
-num_images = 1000
+num_images = 100
 
 with open(csv_path,'w') as csvfile:
 	coord_writer = csv.writer(csvfile, delimiter=',')
 	for i in range(num_images):
 	
+		is_flip = (random.uniform(0,1)<0.5)
+
 		x,y,z = bo.random_shell_coords(5.0)
 		lamp.set_location((x,y,z))
 
 		x,y,z = bo.random_shell_coords(5.0)
 		lamp2.set_location((x,y,z))
 
-		scene.add_object_unfixed(product)
-		scene.add_object_unfixed(cube2)
-
 		x,y,z = bo.random_shell_coords(7.0)
 		cam.set_location((x,y,z))
 		cam.face_towards(0.0,0.0,0.0)
 
+		spin_angle = random.uniform(0.0,360.0)
+		cam.spin(spin_angle)
+
 		loc = bo.random_cartesian_coords(0.0,0.0,0.0,1.0,4.0)
 		product.set_location((loc))
-		
+
 		is_flip = (random.uniform(0,1)<(2./3.))
 		if is_flip:
 			is_flip = (random.uniform(0,1)<0.5)
@@ -97,11 +100,12 @@ with open(csv_path,'w') as csvfile:
 		else:
 			product.set_rot(0,0,1,0)
 			coord_writer.writerow([x,y,z])
-		
+
 		loc2 = loc
 
 		while(list_distances(loc, loc2) < math.sqrt(3)):
 			loc2 = bo.random_cartesian_coords(0.0,0.0,0.0,2.0,4.0)
 
 		cube2.set_location(loc2)
-		scene.render_to_file('D:\\PycharmProjects\\Lobster\\src\\rendering\\9562dwzzz4sg-BottleBeerCorona\\Corona\\render\\render%d.png'%i)
+		render_path = os.path.join(render_folder,'render%d.png'%i)
+		scene.render_to_file(render_path)
