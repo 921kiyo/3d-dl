be14e61ea29ad78ed6310b5ad84b573dd120ff1e
Matthew Wong <matthew@Matthews-MacBook-Air.local>
2018-02-10 21:31:29 +0000

    Added classify.py
    Former-commit-id: b210a8f39b81f8e4e11061d8c254ff3d8a68d2fe

---
+++ keras/keras-official/classify.py
@@ -0,0 +1,55 @@
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras import optimizers
+from time import *
+import os
+from keras.models import load_model
+
+from keras.applications import imagenet_utils
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# initialize the input image shape (224x224 pixels) along with
+# the pre-processing function (this might need to be changed
+# based on which model we use to classify our image)
+inputShape = (224, 224)
+preprocess = preprocess_input
+
+model = load_model('production_model_1.h5')
+
+# load the input image using the Keras helper utility while ensuring
+# the image is resized to `inputShape`, the required input dimensions
+# for the ImageNet pre-trained network
+print("[INFO] loading and pre-processing image...")
+image = load_img(args["image"], target_size=inputShape)
+image = img_to_array(image)
+
+# our input image is now represented as a NumPy array of shape
+# (inputShape[0], inputShape[1], 3) however we need to expand the
+# dimension by making the shape (1, inputShape[0], inputShape[1], 3)
+# so we can pass it through thenetwork
+image = np.expand_dims(image, axis=0)
+
+# pre-process the image using the appropriate function based on the
+# model that has been loaded (i.e., mean subtraction, scaling, etc.)
+image = preprocess(image)
+
+
+
+# classify the image
+print("[INFO] classifying image")
+preds = model.predict(image)
+print(preds)
