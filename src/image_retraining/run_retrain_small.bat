e5f0ca515e26d473a1a91d308e4382fe9b75c42e
Matthew Wong <mzw17@edge23.doc.ic.ac.uk>
2018-02-15 16:35:13 +0000

    Changed test script directory to qlone images

--- src/image_retraining/run_retrain_small.bat
+++ src/image_retraining/run_retrain_small.bat
@@ -1,5 +1,5 @@
 . /vol/project/2017/530/g1753002/ocadovenv/ocadovenv/bin/activate
 exp_dir=/vol/project/2017/530/g1753002/Feasibility
-image_dir=$exp_dir/mvp_training_data_correct/white/
-output_dir=$exp_dir/trained_models/tensorflow_trained/white_model_exp
+image_dir=$exp_dir/mvp_training_data_correct/qlone_images/
+output_dir=$exp_dir/trained_models/tensorflow_trained/qlone_model_exp
 python ./retrain.py --image_dir $image_dir --output_dir $output_dir --how_many_training_steps 500
