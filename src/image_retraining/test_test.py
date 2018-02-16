c25791fa88dcca73df999b6f3e5568690329222c
Kiyohito Kunii <kk3317@gpu04.doc.ic.ac.uk>
2018-02-16 14:46:39 +0000

    add test_test.py

---
+++ src/image_retraining/test_test.py
@@ -0,0 +1,62 @@
+from tensorflow.python.framework import test_util
+import tensorflow as tf
+import test
+
+# 15 unit tests
+
+class TestTest(test_util.TensorFlowTestCase):
+    def test_create_label_lists(self):
+        # result = create_label_lists()
+        self.assertEqual(True, True)
+        pass
+
+    def test_get_test_files(self):
+        pass
+
+    def test_create_model_graph(self):
+        pass
+
+    def test_create_model_info(self):
+        pass
+
+    def test_run_resize_data(self):
+        pass
+
+    def test_adaptive_equalize(self):
+        pass
+
+    def test_tf_equalize(self):
+        pass
+
+    def test_add_jpeg_decoding(self):
+        pass
+
+    def test_run_resize_data(self):
+        pass
+
+    def test_eval_result(self):
+        pass
+
+    def test_extract_summary_tensors(self):
+        pass
+
+    def test_plot_confusion_matrix(self):
+        pass
+
+    def test_get_test_files(self):
+        pass
+
+    def test_compute_sensitivity(self):
+        pass
+
+    def test_compute_precision(self):
+        pass
+
+    def test_plot_bar(self):
+        pass
+
+    def test_summarize_results(self):
+        pass
+
+if __name__ == '__main__':
+  tf.test.main()
