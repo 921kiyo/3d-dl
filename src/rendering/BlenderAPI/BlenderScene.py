cd9c74c0556d68a383f31c2e7d31944a8d443633
MSI\weihan <samuelong168@gmail.com>
2018-03-03 23:53:12 +0000

    initial implementation of RandomScene

--- src/rendering/BlenderAPI/BlenderScene.py
+++ src/rendering/BlenderAPI/BlenderScene.py
@@ -6,6 +6,9 @@ import itertools
 
 from rendering.BlenderAPI.BlenderObjects import *
 from rendering.BlenderAPI.BlenderShapes import *
+from rendering.BlenderAPI.BlenderLamps import BlenderPoint
+import rendering.RandomLib.random_render as rnd
+
 
 class BlenderRoom(object):
     def __init__(self, radius):
@@ -84,6 +87,58 @@ class BlenderScene(object):
     def render_to_file(self, filepath):
         self.data.render.filepath = filepath
         bpy.ops.render.render(write_still=True)
+
+    def remove_subject(self):
+        if self.subject is not None:
+            self.subject.delete()
+
+    def remove_lamps(self):
+        for lamp in self.lamps:
+            lamp.delete()
+
+class BlenderRandomScene(BlenderScene):
+    def __init__(self, data):
+        super(BlenderRandomScene, self).__init__(data)
+        self.params = {}
+        '''light params'''
+        self.params['num_lights']       = {'dist': 'uniform_d', 'l': 1, 'r': 4}
+        self.params['light_dist']       = {'dist': 'trunc_norm', 'mu':5.0, 'sigmu': 1.0, 'l': 0.0, 'r': None}
+        self.params['light_energy']     = {'dist': 'trunc_norm', 'mu': 0.0, 'sigmu': 0.0, 'l': 0.0, 'r': None}
+        '''camera params'''
+        self.params['camera_radius']    = {'dist': 'trunc_norm', 'mu': 6.0, 'sigmu': 0.0, 'l': 0.0, 'r': None}
+        self.params['spin_angle']       = {'dist': 'uniform_c', 'l':0.0, 'r':360.0}
+        self.params['camera_phi_sigma'] = {'dist': 'constant', 'k': 30.0}
+        self.params['subject_size']     = {'dist': 'trunc_norm', 'mu':8.0, 'sigmu': 0.0, 'l':0.0, 'r': None}
+
+    def set_num_lights(self, N):
+        self.num_lights = N
+        self.remove_lamps()
+        for i in range(self.num_lights):
+            self.add_lamp(BlenderPoint(None))
+
+    def load_subject_from_path(self, obj_path, texture_path):
+        self.remove_subject()
+        self.add_subject(BlenderImportedShape(obj_path=obj_path, location=(-1,0,-1) ,orientation=(180,0,1,0)))
+        self.subject.set_mesh_bbvol(self.params['subject_size']['mu'])  # size of original cube
+        self.subject.add_image_texture(texture_path)
+        # texture appearance are fixe for now
+        self.subject.set_diffuse(color=(1, 0, 0, 1), rough=0.1)
+        self.subject.set_gloss(rough=0.1)
+        self.subject.set_mixer(0.3)
+        self.subject.set_location(0., 0., 0.)
+
+    def sample_param_trunc_norm(self, params):
+        return rnd.sample_trunc_norm(params['mu'], params['sigma'], params['l'], params['r'])
+
+    def sample_param_uniformc(self, params):
+        return rnd.uniform(params['l'], params['r'])
+
+    def sample_param_uniformd(self, params):
+        return rnd.randint(params['l'], params['r'])
+
+    def sample_param_norm(self, params):
+        return random.gauss(params['mu'], params['sigma'])
+
 
 
 
