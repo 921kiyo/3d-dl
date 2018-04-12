3ab852455a32910783e5d6231644e1d4111cad3b
MSI\weihan <samuelong168@gmail.com>
2018-04-12 14:02:22 +0100

    prettify plots
    Former-commit-id: 9dc4c96f850cc6965910fb4624014097d47dad00

--- kerasmodels/decision_analysis.py
+++ kerasmodels/decision_analysis.py
@@ -12,6 +12,9 @@ x_incorrect = []
 N = 0
 acc = 0
 
+'''
+Fetch the max scores from the test pickle file
+'''
 for label in per_class_test_results:
     results = per_class_test_results[label]
     for result in results:
@@ -23,6 +26,9 @@ for label in per_class_test_results:
         N += 1.
 acc = acc/N
 
+'''
+Utility functions to compute probabilities
+'''
 def compute_xent(x):
     return -np.log(np.array(x))
 
@@ -49,6 +55,9 @@ def query_posteriors(lambda_pr_0, lambda_pr_1, p0, xq):
     p_0_x, p_1_x = compute_posterior(p_x_0, p_x_1, p0, (1 - p0))
     return p_0_x, p_1_x
 
+'''
+Compute xent and estimate exponential dist params
+'''
 x_correct = compute_xent(x_correct)
 x_incorrect = compute_xent(x_incorrect)
 x = np.append(x_correct, x_incorrect)
@@ -58,12 +67,20 @@ lambda_incorrect = estimate_lambda(x_incorrect)
 
 plt.figure()
 
+'''
+Plot the histrograms of xent vs density histogram
+'''
 plt.subplot(3,1,1)
 n_correct ,_ ,_ = plt.hist(x_correct, bins=30, density=True)
 n_correct.sort()
 p_x_correct = expon.pdf(n_correct, 0, 1./lambda_correct)
 plt.plot(n_correct, p_x_correct)
 plt.xlim([0, 2.0])
+plt.xlabel('max-log-confidence')
+plt.ylabel('PDF')
+plt.legend(['estimated exponential distribution', 'density histogram'])
+plt.title('P max-log-confidence | correct classification')
+plt.grid()
 
 plt.subplot(3,1,2)
 n_incorrect ,_ , _ = plt.hist(x_incorrect, bins=30, density=True)
@@ -71,6 +88,11 @@ n_incorrect.sort()
 p_x_incorrect = expon.pdf(n_incorrect, 0, 1./lambda_incorrect)
 plt.plot(n_incorrect, p_x_incorrect)
 plt.xlim([0, 2.0])
+plt.xlabel('max-log-confidence')
+plt.ylabel('PDF')
+plt.legend(['estimated exponential distribution', 'density histogram'])
+plt.title('P max-log-confidence | incorrect classification')
+plt.grid()
 
 plt.subplot(3,1,3)
 n_all ,_ , _ = plt.hist(x, bins=30, density=True)
@@ -78,14 +100,29 @@ n_all.sort()
 p_x = query_marginals(lambda_correct, lambda_incorrect, acc, n_all)
 plt.plot(n_all, p_x)
 plt.xlim([0, 2.0])
+plt.xlabel('max-log-confidence')
+plt.ylabel('PDF')
+plt.legend(['estimated exponential distribution', 'density histogram'])
+plt.title('P max-log-confidence (marginal)')
+plt.grid()
+
+plt.tight_layout(pad = 0.5)
 
 plt.figure()
 xq = np.linspace(0.0, 1.0, num=100)
 p_correct_x, p_incorrect_x = query_posteriors( lambda_correct, lambda_incorrect, acc, xq)
 plt.plot(xq, p_correct_x, 'g', xq, p_incorrect_x, 'r')
+plt.xlabel('max-log-confidence')
+plt.ylabel('PDF')
+plt.legend(['P(correct | max-log-confidence)', 'P(incorrect | max-log-confidence)'])
+plt.title('Posterior distribution of classifications given max-log-confidence values')
+plt.grid()
 
 plt.show()
 
+'''
+compute the confusion matrix of rejected samples (classified as misclassified)
+'''
 def get_loss(max_score, L, lambda_pr_0, lambda_pr_1, acc):
     x = max_score
     p_0_x, p_1_x = query_posteriors(lambda_pr_0, lambda_pr_1, acc, x)
