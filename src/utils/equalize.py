1497e7662e908bdcaed24ac5ce06e21cf43ed3e2
MSI\weihan <samuelong168@gmail.com>
2018-04-16 12:42:13 +0100

    adjusted equalization constant
    Former-commit-id: a2f5917d28e3aaccdad3cf4403fc848334be5c27

--- src/utils/equalize.py
+++ src/utils/equalize.py
@@ -3,8 +3,8 @@ import os
 from skimage import data, img_as_float, io
 from skimage import exposure
 
-src_folder = 'D:\\PycharmProjects\\manhattan_test_data\\warehouse_test\\StrawberryYogurt'
-dest_folder = 'D:\\PycharmProjects\\manhattan_test_data\\warehouse_test_adap_eq\\StrawberryYogurt'
+src_folder = 'D:\\PycharmProjects\\manhattan_test_data\\warehouse_test\\Anchor'
+dest_folder = 'D:\\PycharmProjects\\manhattan_test_data\\warehouse_test_adap_eq2\\Anchor'
 
 for (dirpath,_,filenames) in os.walk(src_folder):
 
@@ -19,7 +19,7 @@ for (dirpath,_,filenames) in os.walk(src_folder):
         if filename.lower().endswith('.jpg'):
 
             img = io.imread(os.path.join(dirpath,filename))
-            img_eq = exposure.equalize_adapthist(img)
+            img_eq = exposure.equalize_adapthist(img, clip_limit=0.05)
 
             dest_filename = os.path.join(dest_dir, filename)
 
