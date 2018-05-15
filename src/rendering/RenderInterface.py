1123698984e86a5a729a627df144ff3aa270e994
MSI\weihan <samuelong168@gmail.com>
2018-05-15 22:35:41 +0100

    catches exception for non-CUDA devices
    Former-commit-id: 6fb2d74557a4afd9f8ffedf12d167d6f953808f8

--- src/rendering/RenderInterface.py
+++ src/rendering/RenderInterface.py
@@ -62,10 +62,14 @@ class RenderInterface(object):
         controls all rendering
         :return: None
         """
-        C = bpy.context
-        C.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
-        C.user_preferences.addons['cycles'].preferences.devices[0].use = True
-        C.scene.render.engine = 'CYCLES'
+        try:
+            C = bpy.context
+            C.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+            C.user_preferences.addons['cycles'].preferences.devices[0].use = True
+            C.scene.render.engine = 'CYCLES'
+        except:
+            print("Warning: CUDA device not detected, using CPU instead!", file=sys.stderr)
+
         # instantiate scene
         self.scene = bld.BlenderRandomScene(bpy.data.scenes[0])
         # delete the initial cube
